293fc700b93171369535f9f86c0494eac624f776
Joe Yu <joeyu@Joes-MacBook-Pro.local>
2017-08-24 10:31:12 -0700

    updated login MODAL

---
+++ apps/sub_box/static/sub_box/js/main.js
@@ -0,0 +1,16 @@
+
+//  Login MODAL
+
+$(document).ready(function() {
+	$(".call_modal").click(function() {
+		$(".modal").fadeIn();
+		$(".modal_main").show();
+	});
+	$(".close").click(function() {
+		$(".modal").fadeOut();
+		$(".modal_main").fadeOut();
+	});
+
+// Add Logo Hovers Here
+	
+});
