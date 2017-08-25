94aedd1162cca0dc9e7bdc5beab7c168b87c7306
wtran29 <wtran29@gmail.com>
2017-08-25 01:06:48 -0700

    updated carousel, buttons, added sponsors and contacts

--- apps/sub_box/static/sub_box/js/carousel.js
+++ apps/sub_box/static/sub_box/js/carousel.js
@@ -11,14 +11,14 @@ $(document).ready(function(){
 		}
 		currentActiveImage.removeClass("image_shown").addClass("image_hidden").css("z-index", -10);
 		nextActiveImage.addClass("image_shown").removeClass("image_hidden").css("z-index", 20);
-		$(".carousel_inner img").not([currentActiveImage, nextActiveImage]).css("z-index",1);
+		$(".carousel_inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
 
 		e.preventDefault();
 	});
 
 	$(".prev_link").on("click", function(e){
 
-		var currentActiveImage = $(".image-shown");
+		var currentActiveImage = $(".image_shown");
 		var nextActiveImage = currentActiveImage.prev();
 
 		if(nextActiveImage.length == 0)
